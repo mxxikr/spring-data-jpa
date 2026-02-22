bc10aa201fd51c3c0d956e9da27587ddb563a004
mxxikr <mxxikr@gmail.com>
2026-02-22 16:30:22 +0900

    쿼리 메소드 기능: 반환 타입

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -7,6 +7,7 @@ import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
@@ -28,4 +29,5 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query("select m from Member m where m.username in :name")
     List<Member> findByNames(@Param("name") List<String> names);
+
 }
