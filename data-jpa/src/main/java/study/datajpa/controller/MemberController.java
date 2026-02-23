b33f0203233d8d52fac322e07d275a78036b00c2
mxxikr <mxxikr@gmail.com>
2026-02-23 19:54:02 +0900

    확장 기능: Web 확장 - 페이징과 정렬

--- data-jpa/src/main/java/study/datajpa/controller/MemberController.java
+++ data-jpa/src/main/java/study/datajpa/controller/MemberController.java
@@ -1,9 +1,12 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -22,5 +25,18 @@ public class MemberController {
     @GetMapping("/members/{id}")
     public String findMember(@PathVariable("id") Member member) {
         return member.getUsername();
+    }
+
+    @GetMapping("/members")
+    public Page<MemberDto> list(Pageable pageable) {
+//        Page<Member> page = memberRepository.findAll(pageable);
+//        Page<MemberDto> pageDto = page.map(MemberDto::new);
+//        return page;
+        return memberRepository.findAll(pageable).map(MemberDto::new);
+    }
+
+    @RequestMapping(value = "/members_page", method = RequestMethod.GET)
+    public String pageList(@PageableDefault(size = 12, sort = "username", direction = Sort.Direction.DESC) Pageable pageable) {
+        return "";
     }
 }
